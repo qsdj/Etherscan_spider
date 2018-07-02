fe9857177d07532ca02af179b3f5740cb915a978
root <root@localhost.localdomain>
2018-07-02 15:01:33 +0800

    :smile:

---
+++ tokens_code/0x17e67d1cb4e349b9ca4bc3e17c7df2a397a7bb64_Freyr_Coin_(FREC).sol
@@ -0,0 +1,91 @@
+//token_name	0x17e67d1cb4e349b9ca4bc3e17c7df2a397a7bb64_Freyr_Coin_(FREC)
+//token_url	https://etherscan.io/address/0x17e67d1cb4e349b9ca4bc3e17c7df2a397a7bb64#code
+//spider_time	2018/07/02 11:37:00
+//token_Transactions	8811 txns
+//token_price	
+
+pragma solidity 0.4.11;
+
+contract Token {
+    function transfer(address to, uint256 value) returns (bool success);
+    function transferFrom(address from, address to, uint256 value) returns (bool success);
+    function approve(address spender, uint256 value) returns (bool success);
+
+    function totalSupply() constant returns (uint256 supply) {}
+    function balanceOf(address owner) constant returns (uint256 balance);
+    function allowance(address owner, address spender) constant returns (uint256 remaining);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+contract FreyrCoin is Token {
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+
+    string constant public name = "Freyr Coin";
+    string constant public symbol = "FREC";
+    uint8 constant public decimals = 18;
+
+    function FreyrCoin()
+        public
+    {
+        totalSupply = 10000000000 * 10**18;
+        balances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool)
+    {
+        if (balances[msg.sender] < _value) {
+            throw;
+        }
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value)
+        public
+        returns (bool)
+    {
+        if (balances[_from] < _value || allowed[_from][msg.sender] < _value) {
+            throw;
+        }
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        allowed[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool)
+    {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender)
+        constant
+        public
+        returns (uint256)
+    {
+        return allowed[_owner][_spender];
+    }
+
+    function balanceOf(address _owner)
+        constant
+        public
+        returns (uint256)
+    {
+        return balances[_owner];
+    }
+}
